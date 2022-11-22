a115e19cadd830c5f46548efe957c7cf1f3bfccc
Racquel Munoz <racquelg85@hotmail.com>
2022-11-22 17:45:19 -0600

    added the contact form and test

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -6,9 +6,25 @@ import Nav from "..";
 
 afterEach(cleanup);
 
-describe("Nav component", () => {
+describe("Nav component renders", () => {
+  const categories = [
+    { name: "portraits", description: "Portraits of people in my life" },
+  ];
+  const mockCurrentCategory = jest.fn();
+  const mockSetCurrentCategory = jest.fn();
+  const mockContactSelected = jest.fn();
+  const mockSetContactSelected = jest.fn();
+
   it("renders", () => {
-    render(<Nav />);
+    render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
+      />
+    );
   });
 
   it("matches snapshot", () => {
