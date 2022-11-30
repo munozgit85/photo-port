108403ddd2159003b55a254109e75dc765328a29
Racquel Munoz <racquelg85@hotmail.com>
2022-11-29 22:21:15 -0600

    changed the modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -4,16 +4,16 @@ const Modal = ({ onClose, currentPhoto }) => {
   const { name, description, category, index } = currentPhoto;
 
   return (
-    <div className="modalBackdrop">
-      <div className="modalContainer">
-        <h3 className="modalTitle">{name} </h3>
+    <div className="Backdrop">
+      <div className="Container">
+        <h3 className="Title">{name} </h3>
         <img
           src={require(`../../assets/large/${category}/${index}.jpg`).default}
           alt="current category"
         />
         <p>{description}</p>
         <button type="button" onClick={onClose}>
-          Close this modal
+          Close this
         </button>
       </div>
     </div>
